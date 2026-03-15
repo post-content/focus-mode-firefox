3648dc048e50c243457ddc548b7ffac5ab753b12
Brandon Alvendia <66085050+post-content@users.noreply.github.com>
2026-03-15 14:09:08 -0500

    Switch from chrome to browserAPI for compatibility

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
+// Firefox/Chrome compatibility shim
+const browserAPI = window.browser || window.chrome;
+
 const GOOGLE_DOCS_URL = "https://docs.google.com/document/";
 
 const APP_STATES = {
   OFF: { iconSet: "default" },
   LIGHT: { iconSet: "light", requiredCss: ["css/default.css"] },
-  DARK: { iconSet: "dark", requiredCss: ["css/default.css", "css/dark.css"] },
 };
 
 const allCssFiles = Array.from(Object.values(APP_STATES).reduce((files, state) => {
@@ -11,12 +13,12 @@ const allCssFiles = Array.from(Object.values(APP_STATES).reduce((files, state) =
   return files;
 }, new Set()));
 
-const ORDERED_STATES = [APP_STATES.OFF, APP_STATES.LIGHT, APP_STATES.DARK];
+const ORDERED_STATES = [APP_STATES.OFF, APP_STATES.LIGHT];
 
 let currentState = APP_STATES.OFF;
 
 const setIcon = async (state) => {
-  await chrome.action.setIcon({
+  await browserAPI.browserAction.setIcon({
     path: {
       16: `images/icons/${state.iconSet}/icon-16.png`,
       48: `images/icons/${state.iconSet}/icon-48.png`,
@@ -26,11 +28,11 @@ const setIcon = async (state) => {
 };
 
 // set the initial icon
-chrome.runtime.onInstalled.addListener(() => {
+browserAPI.runtime.onInstalled.addListener(() => {
   setIcon(currentState);
 });
 
-chrome.action.onClicked.addListener(async (tab) => {
+browserAPI.browserAction.onClicked.addListener(async (tab) => {
   // If the current tab is not a Google Docs tab, return
   if (!tab.url.startsWith(GOOGLE_DOCS_URL)) {
     return;
@@ -49,7 +51,7 @@ chrome.action.onClicked.addListener(async (tab) => {
   const target = { tabId: tab.id };
 
   // apply the corresponding CSS file
-  await chrome.scripting.removeCSS({ target, files: allCssFiles });
+  await browserAPI.scripting.removeCSS({ target, files: allCssFiles });
   nextState.requiredCss &&
-    (await chrome.scripting.insertCSS({ target, files: nextState.requiredCss }));
+    (await browserAPI.scripting.insertCSS({ target, files: nextState.requiredCss }));
 });
